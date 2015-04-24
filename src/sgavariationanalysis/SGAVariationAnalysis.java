38fe1c9f0637036aed9fc9946426c0f178707d26
Brett Crawford <brettjamescrawford@gmail.com>
2015-04-24 14:50:40 -0400

    Created Netbeans project

---
+++ src/sgavariationanalysis/SGAVariationAnalysis.java
@@ -0,0 +1,22 @@
+/*
+ *  Author: Brett Crawford <brett.crawford@temple.edu>
+ *  File:   SGAVariationAnalysis.java
+ *  Date:   Apr 24, 2015
+ *  Desc:
+ */
+package sgavariationanalysis;
+
+/**
+ *
+ * @author Brett Crawford <brett.crawford@temple.edu>
+ */
+public class SGAVariationAnalysis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
